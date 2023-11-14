799c1f1efafea6145bc5f198181958827569cdde
RiteshShukla77 <riteshmohan5878@gmail.com>
2023-11-14 06:14:30 +0530

    Added a feature to store multiple user data using their serial number of entry as the object

--- Add_User.js
+++ Add_User.js
@@ -1,29 +1,39 @@
 const myform = document.querySelector('#my-form');
-      const name = document.querySelector('#name');
+      const FullName = document.querySelector('#name');
       const email = document.querySelector('#email');
       const msg = document.querySelector('.msg');
       const user = document.querySelector('#users');
+      let i="1";
 
       myform.addEventListener('submit',onsubmit);
       //document.addEventListener('submit',addtostore);
       
       function onsubmit(e){
         e.preventDefault();
-        if(name.value==''||email.value=='')
+        if(FullName.value==''||email.value=='')
         {
-          //alert('Please Enter the Feilds');//
+          //alert('Please Enter the Feilds');
           msg.classList.add('error')
 
           setTimeout(()=> msg.remove(),3000);
           msg.innerHTML="Please Enter All Feilds*"
         }
         else{
-          localStorage.setItem(name.value,email.value);
+          let myobj={ Name: FullName.value ,
+            Email : email.value
+          }
+          let obj_sr = JSON.stringify(myobj);
+          localStorage.setItem("User "+i,obj_sr);
+          let obj_dsr = JSON.parse(localStorage.getItem(i));
+          var a = parseInt(i);
+          a=a+1;
+          i=a.toString();
+
           const li = document.createElement('li');
-          li.appendChild(document.createTextNode(`${name.value} : ${email.value}`));
+          li.appendChild(document.createTextNode(`${FullName.value} : ${email.value}`));
           user.appendChild(li);
           //Clear Feilds
-          name.value='';
+          FullName.value='';
           email.value='';
         }
       }
