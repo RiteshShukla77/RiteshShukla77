f1d3f1e3076859b2c1711672d57d4be1962ebd5e
RiteshShukla77 <riteshmohan5878@gmail.com>
2023-11-15 01:13:26 +0530

    Added new feature to the Appointment page to delete the user infor from list and storage.

--- Add_User.js
+++ Add_User.js
@@ -1,44 +1,50 @@
 const myform = document.querySelector('#my-form');
-      const FullName = document.querySelector('#name');
-      const email = document.querySelector('#email');
-      const msg = document.querySelector('.msg');
-      const user = document.querySelector('#users');
-      const number = document.querySelector('#tel');
-      // let i="1";
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const user = document.querySelector('#users');
+const phone = document.querySelector('#Phone');
 
-      myform.addEventListener('submit',onsubmit);
-      //document.addEventListener('submit',addtostore);
-      
-      function onsubmit(e){
-        e.preventDefault();
-        if(FullName.value==''||email.value==''||number.value=='')
-        {
-          //alert('Please Enter the Feilds');
-          msg.classList.add('error')
 
-          setTimeout(()=> msg.remove(),3000);
-          msg.innerHTML="Please Enter All Feilds*"
-        }
-        else{
-          let myobj={ Name: FullName.value ,
-            Email : email.value,
-            Phone : number.value
-          }
-          let obj_sr = JSON.stringify(myobj);
-          localStorage.setItem(email.value,obj_sr);
-          let obj_dsr = JSON.parse(localStorage.getItem(email.value));
-          console.log(obj_dsr);
-          // var a = parseInt(i);
-          // a=a+1;
-          // i=a.toString();
 
-          const li = document.createElement('li');
-          li.appendChild(document.createTextNode(`${FullName.value} : ${email.value}: ${number.value}`));
-          user.appendChild(li);
-          //Clear Feilds
-          FullName.value='';
-          email.value='';
-          number.value='';
-        }
-      }
-      
+
+
+
+myform.addEventListener('submit',onsubmit);
+
+function onsubmit(e){
+  e.preventDefault();
+  var obj={
+    Name : name.value,
+    Email : email.value,
+    Phone : phone.value
+  }
+  if(name.value==''||email.value==''||phone.value=='')
+  {
+    //alert('Please Enter the Feilds');//
+    msg.classList.add('error')
+
+    setTimeout(()=> msg.remove(),3000);
+    msg.innerHTML="Please Enter All Feilds*"
+  }
+  else{
+    const li = document.createElement('li');
+    const del = document.createElement('button');
+    del.textContent= " Delete ";
+    del.style.backgroundColor = "red";
+    del.className="Delete";
+    del.onclick = () =>{
+      user.removeChild(li);
+      localStorage.removeItem(obj.Email);
+    }
+    li.appendChild(document.createTextNode( obj.Name + ":" + obj.Email + ":" + obj.Phone));
+    li.appendChild(del);
+    user.appendChild(li);
+    localStorage.setItem(obj.Email,JSON.stringify(obj));
+    //Clear Feilds
+    name.value='';
+    email.value='';
+    phone.value='';
+  }
+
+}
